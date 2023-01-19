a0116cc49e7bd1d5414f419ef5d168dc8fc85195
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-19 14:28:46 +0000

    웹툰을 검색하면, 서버에서 타이틀과 타이틀 ID 리스트가 반환되도록 구현함

--- routers/api/v1/webtoon.js
+++ routers/api/v1/webtoon.js
@@ -1,9 +1,13 @@
 import express from "express"
+import axios from "axios"
+import cheerio from "cheerio"
+import url from "url"
+
 
 const router = express.Router()
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const SEARCH_TYPES_TO_CHECK = ["title", "image", "index"]
   
   const {type:SEARCH_TYPE, keyword:SEARCH_KEYWORD} = req.query
@@ -18,25 +22,50 @@ router.get('/', (req, res) => {
     return
   }
 
-  switch(SEARCH_TYPE)
-  {
-    case "title" :
-      if(SEARCH_KEYWORD == null)
-      {
-        res.json({is_error:true, message:"검색할 키워드가 지정되지 않았습니다!"})
-        return
-      }
-      
-      res.json({is_error:false, message:`[MOCK] ${SEARCH_KEYWORD} 키워드에 대한 웹툰명 검색 결과를 반환시킴`})
-      return
-
-    case "index" :
-      res.json({is_error:false, message:"[MOCK] 특정 웹툰에 대한 목차 목록을 반환시킴"})
-      return
+  try {
     
-    case "image" :
-      res.json({is_error:false, message:"[MOCK] 웹툰 이미지에 대한 zip 파일을 다운로드 시킴"})
-      return
+    switch(SEARCH_TYPE)
+    {
+      case "title" :
+        if(SEARCH_KEYWORD == null)
+        {
+          res.json({is_error:true, message:"검색할 키워드가 지정되지 않았습니다!"})
+          return
+        }
+  
+        const REQ_RES = await axios.get(`https://comic.naver.com/search?keyword=${SEARCH_KEYWORD}`)
+        if(REQ_RES.status != 200)
+        {
+          res.json({is_error:true, message:"서버 통신도중 에러가 발생했습니다!"})
+          return
+        }
+
+        const $ = cheerio.load(REQ_RES.data)
+        const TITLE_INFOS = $("div#content div.resultBox:nth-child(2) ul.resultList h5 a")
+              .toArray()
+              .map((e) => {
+                const TITLE = $(e).text()
+                const TITLE_ID = url.parse($(e).attr("href"), true).query.titleId
+                return {title:TITLE, title_id:TITLE_ID} 
+              })
+        res.json({is_error:false, result:TITLE_INFOS})
+        return
+  
+      case "index" :
+        res.json({is_error:false, message:"[MOCK] 특정 웹툰에 대한 목차 목록을 반환시킴"})
+        return
+      
+      case "image" :
+        res.json({is_error:false, message:"[MOCK] 웹툰 이미지에 대한 zip 파일을 다운로드 시킴"})
+        return
+    }
+    
+  }
+  catch(e)
+  {
+    console.log(e)
+    res.json({is_error:true, message:"서버에서 처리 도중 에러가 발생함!"})
+    return
   }
 })
 
