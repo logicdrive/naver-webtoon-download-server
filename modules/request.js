71f22b0bdcf0423863472507c55b24b392728a73
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-19 15:02:11 +0000

    분리된 라이브러리가 자바스크립트 표준 문서를 지키도록 리팩토링함

--- modules/request.js
+++ modules/request.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 class Request
 {
-  // GET 요청한 URL 주소에 대한 HTML 결과를 얻기 위해서
+  /** GET 요청한 URL 주소에 대한 HTML 결과를 얻기 위해서 */
   static async get_For_Html(url)
   {
     return (await axios.get(url)).data
--- static/js/lib/request.js
+++ static/js/lib/request.js
@@ -1,7 +1,7 @@
-// HTTP 요청처리를 간단하게하기 위한 라이브러리
+/** HTTP 요청처리를 간단하게하기 위한 라이브러리 */
 class Request
 {
-  // 특정 HTTP 요청 형태에 대해서 JSON Body로 요청하고, JSON 형태로 데이터를 받기 위해서
+  /** 특정 HTTP 요청 형태에 대해서 JSON Body로 요청하고, JSON 형태로 데이터를 받기 위해서 */
   static async JSON_Request(url, request_type, json_body={})
   {
     const HTTP_REQUEST_TYPES = ["GET", "HEAD", "PUT", "POST", "DELETE", "TRACE", "CONNECT", "OPTIONS"]
