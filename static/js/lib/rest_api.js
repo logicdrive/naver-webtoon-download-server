8cfe475bb067c483ea2c1e92bb37502ddde677a0
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-01-21 15:23:39 +0000

    클라이언트측 rest_api가 서버 요청에 대한 에러 체크를 일관성있게 하도록 리팩토링

--- static/js/lib/rest_api.js
+++ static/js/lib/rest_api.js
@@ -4,9 +4,16 @@ class Rest_Api
   /** 서버에 웹툰명을 검색한 결과를 반환시키기 위해서 */
   static async Search_Webtoon_Titles(keyword)
   {
-    const REQ_RESULT = await Request.JSON_Request(`/api/v1/webtoon?type=title&keyword=${keyword}`, "GET")
-    if(REQ_RESULT.is_error)
-      throw new Error(`에러가 발생했습니다. 에러 메세지 : ${REQ_RESULT.error_message}`)
+    const REQ_RESULT = await Rest_Api.request_With_Error_Check(`/api/v1/webtoon?type=title&keyword=${keyword}`, "GET")
     return REQ_RESULT.result
+  }
+
+  /** 서버 응답을 받기전에 에러여부를 확인해서 예외를 일으키기 위해서 */
+  static async request_With_Error_Check(url, request_type, json_body={})
+  {
+    const REQ_RESULT = await Request.JSON_Request(url, request_type, json_body)
+    if(REQ_RESULT.is_error)
+      throw new Error(`Sorry, Some error was happened...\nError Message : ${REQ_RESULT.message}`)
+    return REQ_RESULT
   }
 }
