07d3c9c967d3f643942db03be706970db35fba78
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-02-01 14:31:33 +0000

    클라이언트 인터페이스 개선/기능 추가 및 수정 : 화수를 직접 추가하고 삭제하는 방식으로 변경함

--- routers/api/v1/webtoon_index.js
+++ routers/api/v1/webtoon_index.js
@@ -3,14 +3,18 @@ import Wrap from "../../../modules/wrap.js"
 import Params_Check from "../../../modules/params_check.js"
 import Webtoon_Api from "../../../modules/webtoon_api.js"
 
-/** 유저가 제공한 웹툰 타이틀 ID의 최대 화수를 반환시키기 위해서 */
 async function get_Router_callback(req, res)
 {
-  Params_Check.para_is_null_or_empty(req.query, ["title_id"])
-  const {title_id:TITLE_ID} = req.query
-
-  const MAX_INDEX = await Webtoon_Api.max_Index(TITLE_ID)
-  res.json({is_error:false, result:MAX_INDEX})
+  Params_Check.para_is_null_or_empty(req.query, ["title_id", "start_index", "end_index"])
+  const TITLE_ID = req.query.title_id
+  const START_INDEX = Number(req.query.start_index)
+  const END_INDEX = Number(req.query.end_index)
+  
+  let index_infos = []
+  for(let index=START_INDEX; index<=END_INDEX; index++)
+    index_infos.push({index:index, name:`${index} 화`})
+  
+  res.json({is_error:false, index_infos:index_infos})
 }
 get_Router_callback = Wrap.wrap_With_Try_Res_Promise(get_Router_callback)
 
--- static/js/core/main.js
+++ static/js/core/main.js
@@ -3,20 +3,20 @@ function main()
   change_Visible_Of_Process_Div([true, false, false])
   document.querySelector("#title_search_form").onsubmit = on_Submit_title_Search_Form
   document.querySelector("#zip_download_button").onclick = on_Click_Zip_Download_Button
-  document.querySelector("#select_all_button").onclick = on_Click_Select_All_Button
+  document.querySelector("#add_index_button").onclick = on_Click_Add_Index
 }
 
 /** 웹툰 제목을 검색할 경우, 그에 대한 검색결과를 출력하기 위해서 */
 async function on_Submit_title_Search_Form(e)
 {
-  change_Visible_Of_Process_Div([true, false, false])
   e.preventDefault()
   
   const TITLE_TO_SEARCH = document.querySelector("#title_search_form input[type='text']").value
   if(TITLE_TO_SEARCH.length == 0) throw new Error("검색할 웹툰명을 입력해주세요!")
 
   document.querySelector("#title_search_result_list").innerHTML = ""
-  document.querySelector("#index_search_result_list").innerHTML = ""
+  Index_Manager.init_Index_Info()
+  change_Visible_Of_Process_Div([true, false, false])
   
   const TITLE_RESULTS = await Rest_Api.search_Webtoon_Titles(TITLE_TO_SEARCH)
   if(TITLE_RESULTS.length == 0) throw new Error("검색결과가 존재하지 않습니다! 검색할 웹툰명이 정확한지 확인해주세요!")
@@ -47,52 +47,106 @@ async function on_Click_Searched_Webtoon_Title(e)
   SELECTED_ELEMENT.classList.add("checked")
   SELECTED_ELEMENT.style.background = "lightgray"
 
-  
-  change_Visible_Of_Process_Div([true, false, false])
-  const TITLE_ID_TO_SEARCH = SELECTED_ELEMENT.getAttribute("title_id")
-  const MAX_INDEX = await Rest_Api.search_Max_Index(TITLE_ID_TO_SEARCH)
-
-  const INDEX_RESULT_HTMLS = Iter.range(MAX_INDEX, 1-1, -1).map((index) => 
-    `<li class="list-group-item webtoon_index_item" title_id="${TITLE_ID_TO_SEARCH}" index="${index}">${index}화</li>`)
-  document.querySelector("#index_search_result_list").innerHTML = INDEX_RESULT_HTMLS.join('\n')
-  Element.add_On_Click_Trigger("#index_search_result_list li", on_Click_Searched_Webtoon_Index)
+  Index_Manager.init_Index_Info()
   change_Visible_Of_Process_Div([true, true, false])
 }
 on_Click_Searched_Webtoon_Title = Wrap.wrap_With_Try_Alert_Promise(on_Click_Searched_Webtoon_Title)
 
-/** 검색된 웹툰 화수를 클릭할 경우, 그 선택을 반영시키기 위해서 */
-async function on_Click_Searched_Webtoon_Index(e)
+
+/** 입력한 화수들에 대한 정보를 다운받을 화수들 목록에 추가시키기 위해서 */
+async function on_Click_Add_Index(_)
 {
-  const SELECTED_ELEMENT = e.target
-  if(SELECTED_ELEMENT.classList.contains("checked")) 
-  {
-    SELECTED_ELEMENT.classList.remove("checked")
-    SELECTED_ELEMENT.style.background = ""
-  }
+  const TITLE_ID = document.querySelector("li.webtoon_title_item[class*='checked']").getAttribute("title_id")
+  const START_INDEX = document.querySelector("input#start_index").value
+  const END_INDEX = document.querySelector("input#end_index").value
+
+  if(START_INDEX.length == 0 && END_INDEX.length == 0)
+    throw new Error("추가시킬 화수를 입력해주세요.")
+
+  if(START_INDEX.length != 0 && END_INDEX.length != 0)
+    await Index_Manager.add_Index_Info_By_Range(Number(START_INDEX), Number(END_INDEX), TITLE_ID)
   else
+    if(START_INDEX.length != 0) await Index_Manager.add_Index_Info(Number(START_INDEX), TITLE_ID)
+    else await Index_Manager.add_Index_Info(Number(END_INDEX), TITLE_ID)
+
+  document.querySelector("input#start_index").value = ""
+  document.querySelector("input#end_index").value = ""
+}
+on_Click_Add_Index = Wrap.wrap_With_Try_Alert_Promise(on_Click_Add_Index)
+
+class Index_Manager
+{
+  static _current_index_infos = []
+
+  static init_Index_Info()
   {
-    SELECTED_ELEMENT.classList.add("checked")
-    SELECTED_ELEMENT.style.background = "lightgray"
+    Index_Manager._current_index_infos = []
+    document.querySelector("#index_search_result_list").innerHTML = ""
+  }
+  
+  static async add_Index_Info_By_Range(start_index, end_index, title_id)
+  {
+    if(end_index < start_index) throw new Error("화수 범위가 올바르지 않습니다.")
+    if(start_index == end_index)
+    {
+      await Index_Manager.add_Index_Info(start_index, title_id)
+      return
+    }
+
+    const INDEX_INFOS = (await Rest_Api.request_With_Error_Check(`/api/v1/webtoon_index?title_id=${title_id}&start_index=${start_index}&end_index=${end_index}`, "GET")).index_infos
+    Index_Manager._update_Index_Result_UI(INDEX_INFOS, title_id)
   }
 
-  const CHECKED_WEBTOON_INFOS = document.querySelectorAll("li.webtoon_index_item[class*='checked']")
-  if(CHECKED_WEBTOON_INFOS.length > 0) change_Visible_Of_Process_Div([true, true, true])
-  else change_Visible_Of_Process_Div([true, true, false])
+  static async add_Index_Info(index, title_id)
+  {
+    const INDEX_INFOS = (await Rest_Api.request_With_Error_Check(`/api/v1/webtoon_index?title_id=${title_id}&start_index=${index}&end_index=${index}`, "GET")).index_infos
+    Index_Manager._update_Index_Result_UI(INDEX_INFOS, title_id)
+  }
+
+  static _update_Index_Result_UI(index_infos, title_id)
+  {
+    const CURRENT_INDEX_INFO_INDEXES = Index_Manager._current_index_infos.map((index_info) => index_info.index)
+    for(let index_info of index_infos)
+    {
+      if(CURRENT_INDEX_INFO_INDEXES.includes(index_info.index)) continue
+      Index_Manager._current_index_infos.push(index_info)
+    }
+    Index_Manager._current_index_infos.sort((info_a, info_b) => info_a.index - info_b.index)
+    
+    const INDEX_RESULT_HTMLS = Index_Manager._current_index_infos.map((index_info) => `<li class="list-group-item webtoon_index_item" title_id="${title_id}" index="${index_info.index}">${index_info.name}</li>`)
+    document.querySelector("#index_search_result_list").innerHTML = INDEX_RESULT_HTMLS.join('\n')
+    Element.add_On_Click_Trigger("li.webtoon_index_item", Index_Manager._on_Click_Searched_Webtoon_Index)
+
+    change_Visible_Of_Process_Div([true, true, true])
+  }
+
+  static _on_Click_Searched_Webtoon_Index(e)
+  {
+    const SELECTED_INDEX = e.target.getAttribute("index")
+    Index_Manager._current_index_infos = Index_Manager._current_index_infos.filter((index_info) => index_info.index != SELECTED_INDEX)
+
+    if(Index_Manager._current_index_infos.length > 0) Index_Manager._update_Index_Result_UI([])
+    else
+    {
+      Index_Manager.init_Index_Info()
+      change_Visible_Of_Process_Div([true, true, false])
+    }
+  }
 }
-on_Click_Searched_Webtoon_Index = Wrap.wrap_With_Try_Alert_Promise(on_Click_Searched_Webtoon_Index)
 
 /** 선택된 웹툰 및 화수들을 .zip로 압축해서 다운받도록하기 위해서 */
 async function on_Click_Zip_Download_Button(_)
 {
-  const CHECKED_WEBTOON_INFOS = document.querySelectorAll("li.webtoon_index_item[class*='checked']")
+  const CHECKED_WEBTOON_INFOS = document.querySelectorAll("li.webtoon_index_item")
   if(CHECKED_WEBTOON_INFOS.length == 0)
     throw new Error("다운로드 받을 웹툰 및 화수를 선택해주세요 !")
 
   document.querySelector("#download_process_list").innerHTML = `<li class="list-group-item">다운로드 진행중..</li>`
   CHECKED_WEBTOON_INFOS.forEach((sel) => sel.click())
-  
+
+  const TITLE_ID = document.querySelector("li.webtoon_title_item[class*='checked']").getAttribute("title_id")
   const WEBTOON_INFOS_TO_DOWNLOAD = Object.values(CHECKED_WEBTOON_INFOS).map((sel) => {
-      return {title_id:sel.getAttribute("title_id"), index:sel.getAttribute("index")}
+      return {title_id:TITLE_ID, index:sel.getAttribute("index")}
   })
   const ZIP_DATA_URL = await Rest_Api.data_Url_From_Webtoons_Zip(WEBTOON_INFOS_TO_DOWNLOAD)
   Browser.download_File(ZIP_DATA_URL, "download.zip")
@@ -103,14 +157,6 @@ async function on_Click_Zip_Download_Button(_)
   else change_Visible_Of_Process_Div([true, true, false])
 }
 on_Click_Zip_Download_Button = Wrap.wrap_With_Try_Alert_Promise(on_Click_Zip_Download_Button)
-
-/** 모든 화수를 일괄적으로 선택하기 위해서 */
-async function on_Click_Select_All_Button(_)
-{
-  const UNCHECKED_SELS = document.querySelectorAll("li.webtoon_index_item:not([class*='checked'])")
-  if(UNCHECKED_SELS.length > 0) UNCHECKED_SELS.forEach((sel) => sel.click())
-  else document.querySelectorAll("li.webtoon_index_item[class*='checked']").forEach((sel) => sel.click())
-}
 
 /** 주어진 리스트의 순서에따라서 각 프로세스 블럭의 표시여부를 변경시키기 위해서 
  *
