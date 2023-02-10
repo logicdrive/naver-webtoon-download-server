7f94cd65537036bdc44b85fb9f2f6e6f7a050fbc
sinsung6722 <122352103+logicdrive@users.noreply.github.com>
2023-02-10 11:36:32 +0000

    이미지 다운로드 기능/리팩토링 : zip DATA URL을 얻는 부분을 따로 추출시킴

--- modules/webtoon_service.js
+++ modules/webtoon_service.js
@@ -4,11 +4,41 @@ import Request from "./request.js"
 import Promise_Lib from "./promise.js"
 import Dictionary_Array from "./dictionary_array.js"
 import Iter from "./iter.js"
+import UUID from "./uuid.js"
+import System from "./system.js"
 
 /** webtoon_api를 활용한 더 고수준의 행위들을 추상화시키기 위한 라이브러리 */
 class Webtoon_Service
 {
-  /** 주어진 title_id, index에 매칭되는 웹툰 이미지들을 다운받아서 특정 폴더에 저장시킴 */
+  /** 주어진 웹툰 정보들을 기반으로 이미지들을 다운로드시킨 zip 압축 파일 Data Url을 반환받기 위해서 */
+  static async webtoon_Images_Zip_Data_Url(webtoon_infos, parallel_degree=10)
+  {
+    const FOLDER_UUID = UUID.get_UUID()
+    const DOWNLOAD_FOLDER_PATH = `./downloads/${FOLDER_UUID}`
+    fs.mkdirSync(DOWNLOAD_FOLDER_PATH)
+    
+    for(let webtoon_info_index=0; webtoon_info_index<webtoon_infos.length; webtoon_info_index++)
+    {
+      const WEBTOON_INFO = webtoon_infos[webtoon_info_index]
+      const INDEX_DOWNLOAD_FOLDER_PATH = `${DOWNLOAD_FOLDER_PATH}/${WEBTOON_INFO.directory_name}`
+      fs.mkdirSync(INDEX_DOWNLOAD_FOLDER_PATH)
+  
+      console.log(`[*] ${webtoon_info_index}/${webtoon_infos.length-1} 인덱스 화 다운로드 시도중...`)
+      await Webtoon_Service.download_Webtoon_Images(WEBTOON_INFO.title_id, WEBTOON_INFO.index, INDEX_DOWNLOAD_FOLDER_PATH, parallel_degree)
+      if(webtoon_info_index != webtoon_infos.length-1) { await System.sleep(1000) }
+    }
+    const ZIP_PATH = `./downloads/${FOLDER_UUID}.zip`
+    await System.execute_Shell_Command(`cd ${DOWNLOAD_FOLDER_PATH};zip -r ../${FOLDER_UUID}.zip ./*`)
+  
+    const ZIP_DATA_BASE64 = fs.readFileSync(ZIP_PATH, {encoding: 'base64'})
+    fs.rmSync(DOWNLOAD_FOLDER_PATH, {recursive: true, force: true})
+    fs.rmSync(ZIP_PATH, {force: true})
+
+    const ZIP_DATA_URL = "data:file/zip;base64," + ZIP_DATA_BASE64
+    return ZIP_DATA_URL
+  }
+  
+  /** 주어진 title_id, index에 매칭되는 웹툰 이미지들을 다운받아서 특정 폴더에 저장시키기 위해서 */
   static async download_Webtoon_Images(title_id, index, save_folder_path, parallel_degree=10)
   {
     const IMAGE_LINKS = await Webtoon_Api.get_Image_Links(title_id, index)
--- routers/api/v1/webtoon_image.js
+++ routers/api/v1/webtoon_image.js
@@ -2,10 +2,6 @@ import express from "express"
 import Wrap from "../../../modules/wrap.js"
 import Params_Check from "../../../modules/params_check.js"
 import Webtoon_Service from "../../../modules/webtoon_service.js"
-import System from "../../../modules/system.js"
-
-import UUID from "../../../modules/uuid.js"
-import fs from "fs"
 
 /** 유저가 요청한 특정 웹툰 화수들에 대한 다운로드 서비스를 제공하기 위해서 */
 async function post_Router_callback(req, res)
@@ -13,28 +9,7 @@ async function post_Router_callback(req, res)
   Params_Check.para_is_null_or_empty(req.body, ["webtoon_infos"])
   const {webtoon_infos:WEBTOON_INFOS} = req.body
 
-  const FOLDER_UUID = UUID.get_UUID()
-  const DOWNLOAD_FOLDER_PATH = `./downloads/${FOLDER_UUID}`
-  fs.mkdirSync(DOWNLOAD_FOLDER_PATH)
-  
-  for(let webtoon_info_index=0; webtoon_info_index<WEBTOON_INFOS.length;  webtoon_info_index++)
-  {
-    const WEBTOON_INFO = WEBTOON_INFOS[webtoon_info_index]
-    const INDEX_DOWNLOAD_FOLDER_PATH = `${DOWNLOAD_FOLDER_PATH}/${WEBTOON_INFO.directory_name}`
-    fs.mkdirSync(INDEX_DOWNLOAD_FOLDER_PATH)
-
-    console.log(`[*] ${webtoon_info_index}/${WEBTOON_INFOS.length-1} 인덱스 화 다운로드 시도중...`)
-    await Webtoon_Service.download_Webtoon_Images(WEBTOON_INFO.title_id, WEBTOON_INFO.index, INDEX_DOWNLOAD_FOLDER_PATH)
-    if(webtoon_info_index != WEBTOON_INFOS.length-1) { await System.sleep(1000) }
-  }
-  const ZIP_PATH = `./downloads/${FOLDER_UUID}.zip`
-  await System.execute_Shell_Command(`cd ${DOWNLOAD_FOLDER_PATH};zip -r ../${FOLDER_UUID}.zip ./*`)
-
-  const ZIP_DATA_BASE64 = fs.readFileSync(ZIP_PATH, {encoding: 'base64'})
-  const ZIP_DATA_URL = "data:file/zip;base64," + ZIP_DATA_BASE64
-
-  fs.rmSync(DOWNLOAD_FOLDER_PATH, {recursive: true, force: true})
-  fs.rmSync(ZIP_PATH, {force: true})
+  const ZIP_DATA_URL = await Webtoon_Service.webtoon_Images_Zip_Data_Url(WEBTOON_INFOS)
   res.json({is_error:false, data_url:ZIP_DATA_URL})
 }
 post_Router_callback = Wrap.wrap_With_Try_Res_Promise(post_Router_callback)
